b9e26af2d1a6d250589ce24e70ba12d10b74bcc5
nkmishra217 <deeksha.mishra_cs18@gla.ac.in>
2020-02-22 15:15:13 +0530

    creating abstract method to add, delete,search contacts

--- assignment1/src/adt/ADTImplementation.java
+++ assignment1/src/adt/ADTImplementation.java
@@ -1,5 +1,14 @@
 package adt;
 
 public interface ADTImplementation {
+    // creating a abstract method to add , delete, search, view and exit contacts
+    public void add();
 
+    public void delete();
+
+    public void search();
+
+    public void view();
+
+    public void exit();
 }
